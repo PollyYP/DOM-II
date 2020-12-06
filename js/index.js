3c06066ab79e9d6387d69400f55aadaff04f8385
Polly Yospan <porgroovy@gmail.com>
2020-12-06 01:11:14 -0500

    Three out of ten done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+let funBus = document.querySelector("h1");
+funBus.addEventListener("mouseover", () => {
+  funBus.style.display = "flex";
+  funBus.style.justifyContent = "center";
+});
+
+funBus.addEventListener("mouseleave", () => {
+  funBus.style.display = "";
+  funBus.style.justifyContent = "";
+});
+
+let createBtn = document.querySelectorAll("div.btn");
+createBtn.forEach((e) => (e.style.borderStyle = "outset"));
+createBtn.forEach((e) => (e.style.display = "inline"));
+createBtn.forEach((e) => e.addEventListener("click", displayBtn));
+
+function displayBtn() {
+  alert("Hooray, You're all signed up!");
+  this.style.backgroundColor = "lightGrey";
+  this.textContent = "Done";
+}
